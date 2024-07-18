0f27dfc963e0552cf6de550b2f8e2f97f01cd149
Enrique Greene <102440970+15egreene@users.noreply.github.com>
2024-07-18 19:01:56 -0400

    test

--- project/todos/models.py
+++ project/todos/models.py
@@ -6,3 +6,6 @@ class Todo(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     completed = models.BooleanField(default=False)
     profile_pic = models.ImageField(null=True, blank=True)
+
+
+
