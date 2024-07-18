cc925c9fd4c1142a62e1c00d695f47ddf7decb42
Enrique Greene <102440970+15egreene@users.noreply.github.com>
2024-07-18 11:08:51 -0400

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = 'django-insecure-lkepzbjox_c!xs=quf5p@3^5#m@%sho&f6s_^pk_a&je)_j$gw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://localhost:8000', 'http://127.0.0.1:8000', '18.222.206.250', '18.222.206.250:8000']
+ALLOWED_HOSTS = ['localhost', '18.222.206.250', '18.222.206.250:8000']
 
 
 
@@ -72,12 +72,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://18.222.206.250",
-    "http://18.222.206.250:8000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://18.222.206.250",
+#     "http://18.222.206.250:8000",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+# ]
 
 ROOT_URLCONF = 'project.urls'
 
