293c895c4be90289b20810552adc7067806193b8
Enrique Greene <102440970+15egreene@users.noreply.github.com>
2024-07-18 19:17:59 -0400

    test

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = 'django-insecure-lkepzbjox_c!xs=quf5p@3^5#m@%sho&f6s_^pk_a&je)_j$gw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.191.227.108']
+ALLOWED_HOSTS = ['18.191.227.108', 'dmockup.vercel.app']
 
 
 # Application definition
