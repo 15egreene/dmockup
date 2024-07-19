1b244b827f8815cedb206ca59b3f14d0a5eb3f43
Enrique Greene <102440970+15egreene@users.noreply.github.com>
2024-07-19 18:55:56 -0400

    Update server.js

--- project/frontend/api/server.js
+++ project/frontend/api/server.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+const api = axios.create({
+    baseURL: 'https://dmockup.vercel,app',
+})
 module.exports = async (req, res) => {
     try {
-        const response = await axios.post('/api/todos/', req.body);
+        const response = await api.post('/api/todos/', req.body);
         res.status(200).json(response.data);
     } catch (error) {
         res.status(500).json({ error: error.message });
