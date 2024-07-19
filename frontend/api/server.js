ec4e106680aeb3ebfab93636fa21c74f2f94d60c
Enrique Greene <102440970+15egreene@users.noreply.github.com>
2024-07-18 20:00:40 -0400

    Update server.js

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 module.exports = async (req, res) => {
     try {
-        const response = await axios.post('/api/todos/', req.body);
+        const response = await axios.post('http://18.191.227.108:8000', req.body);
         res.status(200).json(response.data);
     } catch (error) {
         res.status(500).json({ error: error.message });
